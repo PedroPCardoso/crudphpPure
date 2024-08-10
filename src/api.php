d36cf70b161fb3006eff92ad37d1c973e62ebd6f
Pedro Cardoso <pedroecomp@gmail.com>
2024-08-10 19:22:07 -0300

    feat - cors

--- src/api.php
+++ src/api.php
@@ -5,6 +5,9 @@ require_once 'Controllers/OrderController.php';
 require_once 'Controllers/AuthController.php';
 require_once 'Middleware/AuthMiddleware.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
 $authMiddleware = new AuthMiddleware();
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
--- src/index.php
+++ src/index.php
@@ -2,6 +2,14 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/Database/Database.php';
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Authorization');
+if (@$_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    die();
+}
+
+
 Database::getInstance();
 // Carrega as rotas
 require_once 'api.php';
