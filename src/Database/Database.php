487722a1dd0659849d7f7bd667e5608813eef8a3
Pedro Cardoso <pedroecomp@gmail.com>
2024-08-10 17:10:30 -0300

    feat - database

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -1,24 +1,28 @@
 <?php
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+
 class Database
 {
     private static $instance = null;
-    private $pdo;
 
     private function __construct()
     {
-        // Usar o nome do serviço definido no Docker Compose
-        $dsn = 'mysql:host=db;dbname=app_db';
-        $username = 'user';
-        $password = 'password';
+        $capsule = new Capsule;
 
-        try {
-            $this->pdo = new PDO($dsn, $username, $password);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo 'Connection failed: ' . $e->getMessage();
-            exit();
-        }
+        $capsule->addConnection([
+            'driver' => 'mysql',
+            'host' => 'db', // Nome do serviço no docker-compose
+            'database' => 'app_db',
+            'username' => 'user',
+            'password' => 'password',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ]);
+
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
     }
 
     public static function getInstance()
@@ -27,12 +31,6 @@ class Database
             self::$instance = new self();
         }
 
-        return self::$instance->pdo;
-    }
-
-    // Se você precisar do método __wakeup, defina-o como público
-    public function __wakeup()
-    {
-        // lógica para o método __wakeup, se necessário
+        return self::$instance;
     }
 }
